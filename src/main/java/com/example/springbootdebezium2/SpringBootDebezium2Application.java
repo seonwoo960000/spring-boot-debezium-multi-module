085d1d0000922765e4acd23a5445fa300b299081
Kim Seon Woo <seonwoo960000@naver.com>
2022-11-07 21:27:11 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootdebezium2/SpringBootDebezium2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootdebezium2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDebezium2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDebezium2Application.class, args);
+    }
+
+}
